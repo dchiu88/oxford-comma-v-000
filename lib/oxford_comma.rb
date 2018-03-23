be499a91a1f10541bc8ac59651979561f01b61be
David Chiu <davidwchiu@gmail.com>
2018-03-23 04:54:08 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.insert(-2, 'and')
+    array.join(" ")
+  elsif array.length == 3
+    array.insert(-2, 'and')
+    str = array.join(', ')
+    newArr = str.split
+    newArr[-2] = 'and'
+    newArr.join(" ")
+  else
+  result = "#{array[0, array.length].join(', ')} and #{array.last}"
+end
+  end
 end
